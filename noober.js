aa9a2b4d41be2e57809fe9cb701787d2d52f5ad6
Nicolas Morin <nicolas.morin@kellogg.northwestern.edu>
2021-01-31 12:38:01 -0600

    Displays Service and Passengers

--- noober.js
+++ noober.js
@@ -1,21 +1,22 @@
+// writes the returned JSON to the console
+
+// 🔥 start here: write code to loop through the rides
+
+let passengerName;
+let passengerPhone;
+let passengerNumberOfPassengers;
+let passengerPickupAddressLine1;
+let passengerPickupAddressLine2;
+let passengerDropoffAddressLine1;
+let passengerDropoffAddressLine2;
+let levelOfService;
+let outputElement = document.querySelector(".rides");
+
 async function pageLoaded() {
   let response = await fetch("https://kiei451.com/api/rides.json");
   let json = await response.json();
 
-  // writes the returned JSON to the console
   console.dir(json);
-
-  // 🔥 start here: write code to loop through the rides
-
-  let passengerName;
-  let passengerPhone;
-  let passengerNumberOfPassengers;
-  let passengerPickupAddressLine1;
-  let passengerPickupAddressLine2;
-  let passengerDropoffAddressLine1;
-  let passengerDropoffAddressLine2;
-  let levelOfService;
-  let outputElement = document.querySelector(".rides");
 
   for (let i = 0; i < json.length; i++) {
     if (json[i].length > 1) {
@@ -25,7 +26,7 @@ async function pageLoaded() {
         `
         <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
         <i class="fas fa-car-side"></i>
-        <span>${$levelOfService}</span>
+        <span>${levelOfService}</span>
         </h1>`
       );
 
